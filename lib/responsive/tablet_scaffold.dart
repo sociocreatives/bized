1125ce7f4794a92fa32fe49f907587fcd8e3a7ad
Ovasis Group <asd@asds-MacBook-Pro.local>
2024-06-15 10:44:16 +0300

    first commit

--- lib/responsive/tablet_scaffold.dart
+++ lib/responsive/tablet_scaffold.dart
@@ -1,5 +1,8 @@
 // responsive/tablet_scaffold.dart
+import 'package:bized/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/svg.dart';
 
 class TabletScaffold extends StatefulWidget {
   const TabletScaffold({super.key});
@@ -8,15 +11,37 @@ class TabletScaffold extends StatefulWidget {
   State<TabletScaffold> createState() => _TabletScaffoldState();
 }
 
-class _TabletScaffoldState extends State<TabletScaffold> {
+class _TabletScaffoldState extends State<TabletScaffold>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(const Duration(seconds: 4), () {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => WelcomeScreen()));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.red,
-      body: SafeArea(
-          child: Column(
-        children: [Text("data")],
-      )),
+    return Scaffold(
+      backgroundColor: const Color(0xFF0057FF),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(child: SvgPicture.asset('assets/images/bized_logo.svg'))
+        ],
+      ),
     );
   }
 }
